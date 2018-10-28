639c342f96e6d99575e3575499f15b0fa79e55fa
Walker <walkerchan008@gmail.com>
2018-10-28 23:55:05 +0800

    library mini program start up tutorial

--- library/config.js
+++ library/config.js
@@ -1,9 +1,9 @@
-// var requestUrl = 'http://localhost';
+var requestUrl = 'http://localhost';
 // var requestUrl = 'http://192.168.253.1';   // 360 WIFI
 
 
 // var requestUrl = 'http://193.112.187.183';    // 云服务器公网IP
-var requestUrl = 'https://lib.istarmcgames.com';
+// var requestUrl = 'https://lib.istarmcgames.com';
 
 
 module.exports = requestUrl;
